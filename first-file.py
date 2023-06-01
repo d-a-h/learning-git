a174b829c46316ffcbb35474124a3b3c8e502c0c
d-a-h <arshad.chotu10@gmail.com>
2023-06-01 15:07:29 +0530

    commiting for next time

--- first-file.py
+++ first-file.py
@@ -1,3 +1,4 @@
 print("Hey, I am learning git. And this is my first file I am committing and pushing to github")
 print('This is arshad , committing for second time')
-print('This is arshd , commiting for third time')
+print('This is arshd , commiting for third ')
+print('commiting for third time')
